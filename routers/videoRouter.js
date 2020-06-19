d1e9ef4c14da16b39ca52fb98539f96230b8a04c
C4stleB <46134771+C4stleB@users.noreply.github.com>
2020-06-19 13:23:45 +0900

    mongodb done

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,21 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, videoDetail, editVideo, deleteVideo, postUpload } from "../controllers/videoController";
+import { getUpload, videoDetail, deleteVideo, postUpload, postEditVideo, getEditVideo } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
-
+videoRouter.post(routes.upload, uploadVideo, postUpload);
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), getEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
