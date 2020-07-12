af45d282039fa2b4e38358d089190192f48095d4
C4stleB <46134771+C4stleB@users.noreply.github.com>
2020-07-13 00:26:33 +0900

    url

--- src/passport.js
+++ src/passport.js
@@ -12,7 +12,9 @@ passport.use(
         {
             clientID: process.env.GH_ID,
             clientSecret: process.env.GH_SECRET,
-            callbackURL: `http://localhost:4000${routes.githubCallback}`
+            callbackURL: process.env.PRODUCTION ? 
+            `https://polar-sea-27980.herokuapp.com${routes.githubCallback}` : 
+            `http://localhost:4000${routes.githubCallback}`
         },
     githubLoginCallback
     )    
@@ -23,7 +25,9 @@ passport.use(
         {
             clientID: process.env.FB_ID,
             clientSecret: process.env.FB_SECRET,
-            callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+            callbackURL: process.env.PRODUCTION ? 
+            `https://polar-sea-27980.herokuapp.com${routes.facebookCallback}` : 
+            `http://localhost:4000${routes.facebookCallback}`,
             profileFields: ['id', 'displayName', 'email'],
             scope: ['public_profile', 'email']
         }, 
