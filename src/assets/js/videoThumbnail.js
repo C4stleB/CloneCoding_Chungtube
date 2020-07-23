e1f42287fc3476a4281bcf7fd3894dfbb04ce0bd
C4stleB <46134771+C4stleB@users.noreply.github.com>
2020-07-24 05:35:29 +0900

    auto-push

--- src/assets/js/videoThumbnail.js
+++ src/assets/js/videoThumbnail.js
@@ -3,18 +3,18 @@ const videoBlockPlayer = document.querySelectorAll("#jsVideoBlock video");
 
 function handleAutoPlay(event) {
     event.target.play();
-    setInterval(() => event.target.currentTime = 30, 5000);
+    setInterval(() => event.target.currentTime = 35, 5000);
   }
 
 function handleAutoEnd(event) {
     event.target.pause();
-    event.target.currentTime = 30;
+    event.target.currentTime = 35;
   }
 
 function init(){
     videoBlockPlayer.forEach(function(player){
       player.volume = 0;
-      player.currentTime = 30;
+      player.currentTime = 35;
       player.addEventListener("mouseover", handleAutoPlay);
       player.addEventListener("mouseout", handleAutoEnd);
     });
